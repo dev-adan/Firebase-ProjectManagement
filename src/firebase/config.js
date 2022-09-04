a11fa838f0126c31aa6a480828f002674f35d3e3
Adan <adanaligakhar@gmail.com>
2022-09-04 16:05:24 +0500

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqj60kqq-XFpyUxufyCAOZKH4pAOHj0-k",
+  authDomain: "thedojosite-5d879.firebaseapp.com",
+  projectId: "thedojosite-5d879",
+  storageBucket: "thedojosite-5d879.appspot.com",
+  messagingSenderId: "442371744615",
+  appId: "1:442371744615:web:45ed33f4affffdc1d645c2"
+};
+
